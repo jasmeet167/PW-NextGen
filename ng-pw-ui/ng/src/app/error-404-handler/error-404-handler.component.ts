3034486bda5f5aa708f1c188e62404fba937d606
pzmarlic <pzmarlic@csc.com>
2017-03-02 10:07:27 -0500

    Misc changes

--- ng-pw-ui/ng/src/app/error-404-handler/error-404-handler.component.ts
+++ ng-pw-ui/ng/src/app/error-404-handler/error-404-handler.component.ts
@@ -5,10 +5,19 @@ import { Router } from '@angular/router';
   selector: 'app-error-404-handler',
   template: `
     <p-panel header="Navigation Error">
-      <div><h3>Page not Found</h3></div>
-      <div>The provided URL was incorrect, or there is an application problem.</div>
-      <div style="margin-bottom: 30px;">Either way, the system is unable to complete the navigation.</div>
-      <button pButton class="ui-button-info" type="button" (click)="onclick()" label="Return" icon="fa-arrow-left"></button>
+      <div class="ui-g">
+        <div class="ui-g-1" style="display:flex; align-items: center;">
+          <img src="../../assets/img/messagebox_critical.png">
+        </div>
+        <div class="ui-g-11">
+          <div><h2>Page not Found</h2></div>
+          <div>The provided URL was incorrect, or there is an application problem.</div>
+          <div style="margin-bottom: 30px;">Either way, the system is unable to complete the navigation.</div>
+          <div>
+            <button pButton class="ui-button-info" type="button" (click)="onclick()" label="Return" icon="fa-arrow-left"></button>
+          </div>
+        </div>
+      </div>
     </p-panel>
   `
 })
